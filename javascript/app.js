c153901ecf2af64e7d14226e4d0c4def224fe204
Diego Castillo <31873735+Raigiku@users.noreply.github.com>
2018-03-25 14:06:14 -0500

    SABER MAYOR DE FILAS Y COLUMNAS

--- javascript/app.js
+++ javascript/app.js
@@ -21,19 +21,68 @@ document.getElementById('Generar').addEventListener('click', e => {
         }
         var _tr = document.getElementsByTagName('tr');
 
+        console.log('filas: ' + filas);
+        console.log('columnas: ' + columnas);
+
         // inicio: GENERADOR DE ELEMENTOS ALEATORIOS
-        var elementos = [];
-        for (let i = 0; i < _tr.length - 1; ++i) {
+        var elementos = new Array(filas);
+        for (let i = 0; i < filas; ++i) {
+            elementos[i] = new Array(columnas);
+        }
+        for (let i = 0; i < filas; ++i) {
             for (let j = 0; j < columnas; ++j) {
                 var elemento = { x: Math.floor(Math.random() * 11), y: Math.floor(Math.random() * 11) };
-                elementos.push(elemento);
+                elementos[i][j] = elemento;
             }
         }
-        // Debug
-        elementos.forEach(element => {
-            console.log(element);
-        });
+        console.log(elementos);
         // fin: GENERADOR DE ELEMENTOS ALEATORIOS
+        // inicio: ENCONTRAR EQUILIBRIO DE NASH EN ESTRATEGIA PURA
+        var mayoresFila = [];
+        var mayoresColumna = [];
+        var mayorFila;
+        var mayorColumna;
+        // ----inicio: SUBRAYAR
+        // Recorrer por columnas
+        for (let i = 0; i < filas; ++i) {
+            for (let j = 0; j < columnas; ++j) {
+                if (j == 0)
+                    mayorFila = elementos[i][j].y;
+                if (mayorFila < elementos[i][j].y)
+                    mayorFila = elementos[i][j].y;
+            }
+            mayoresFila.push(mayorFila);
+        }
+        console.log('filas: ' + mayoresFila);
+        // Recorrer por filas
+        for (let j = 0; j < columnas; ++j) {
+            for (let i = 0; i < filas; ++i) {
+                if (i == 0)
+                    mayorColumna = elementos[i][j].x;
+                if (mayorColumna < elementos[i][j].x)
+                    mayorColumna = elementos[i][j].x;
+            }
+            mayoresColumna.push(mayorColumna);
+        }
+        console.log('columnas: ' + mayoresColumna);
+        // ----fin: SUBRAYAR
+        // ----inicio: ENCONTRAR RESPUESTA
+        // Recorrer por columnas
+        for (let i = 0; i < filas; ++i) {
+            for (let j = 0; j < columnas; ++j) {
+                if (elementos[i][j].y == mayoresFila[i])
+                    console.log('indice fila: ' + j);
+            }
+        }
+        // Recorrer por filas
+        for (let j = 0; j < columnas; ++j) {
+            for (let i = 0; i < filas; ++i) {
+                if (elementos[i][j].x == mayoresColumna[j])
+                    console.log('indice columna: ' + i);
+            }
+        }
+        // ----fin: ENCONTRAR RESPUESTA
+        // fin: ENCONTRAR EQUILIBRIO DE NASH EN ESTRATEGIA PURA
 
         for (let i = 0; i < _tr.length; ++i) {
             for (let j = 0; j < columnas + 1; ++j) {
