ea6a148f76da9ef0c5d903d9fdc6db60788aefe1
Willington <willingtonortizmaurtua@hotmail.com>
2018-03-25 23:05:46 -0500

    optimizacion de los estilos, optimizacion del javascript, funcionalidad de agregar los contenidos en las celdas

--- javascript/app.js
+++ javascript/app.js
@@ -10,34 +10,79 @@ document.getElementById('Generar').addEventListener('click', e => {
 
     if (!TieneTabla) {
         TieneTabla = true;
+        // Declaración y definición de variables
         var filas = parseInt(document.getElementById('filas').value);
         var columnas = parseInt(document.getElementById('columnas').value);
+        // Se crea la tabla de contenidos
         var table = document.createElement('table');
-        table.setAttribute('id', 'tabla');
+
+        // Se le agrega a la tabla un ID
+        table.setAttribute('id', 'miTabla');
+
+        // Se agrega la tabla al body
         body.appendChild(table);
+
         for (let i = 0; i < filas + 1; i++) {
             var tr = document.createElement('tr');
             table.appendChild(tr);
-        }
-        var _tr = document.getElementsByTagName('tr');
-
-        console.log('filas: ' + filas);
-        console.log('columnas: ' + columnas);
-
-        // inicio: GENERADOR DE ELEMENTOS ALEATORIOS
-        var elementos = new Array(filas);
-        for (let i = 0; i < filas; ++i) {
-            elementos[i] = new Array(columnas);
-        }
-        for (let i = 0; i < filas; ++i) {
-            for (let j = 0; j < columnas; ++j) {
-                var elemento = { x: Math.floor(Math.random() * 11), y: Math.floor(Math.random() * 11) };
-                elementos[i][j] = elemento;
+            for (let j = 0; j < columnas + 1; ++j) {
+                if (i == 0) {
+                    var th = document.createElement('th');
+                    tr.appendChild(th);
+                    if (j != 0) {
+                        th.textContent = "Opcion " + j;
+                    }
+                }
+                else {
+                    var td = document.createElement('td');
+                    tr.appendChild(td)
+                    if (j == 0) {
+                        td.textContent = "Opcion " + i;
+                    }
+                }
             }
         }
-        console.log(elementos);
-        // fin: GENERADOR DE ELEMENTOS ALEATORIOS
-        // inicio: ENCONTRAR EQUILIBRIO DE NASH EN ESTRATEGIA PURA
+        // console.log('filas: ' + filas);
+        // console.log('columnas: ' + columnas);
+
+        /* ===== inicio: GENERADOR DE ELEMENTOS ALEATORIOS ===== */
+
+        // Se crea una matriz con los datos
+        // Primero se crea el arreglo de columnas
+        var elementos = new Array(filas);
+        for (let i = 0; i < filas; ++i) {
+            // Luego se crea el arreglo para cada columna
+            elementos[i] = new Array(columnas);
+            // Después se generan los x e y random
+            for (let j = 0; j < columnas; ++j) {
+                var elemento = { x: Math.floor(Math.random() * 11),
+                                 y: Math.floor(Math.random() * 11) };
+                elementos[i][j] = elemento;
+                // console.log("elemento[" + i + "][" + j + "] = " + elemento.x + ", " + elemento.y);
+
+            }
+        }        
+        /* ===== fin: GENERADOR DE ELEMENTOS ALEATORIOS ===== */
+
+        /* ===== inicio: llenar la tabla ===== */
+            // Obtener una referencia a la tabla
+            var miTabla = document.getElementById('miTabla');
+            // Obtener todas las filas de la tabla
+            var tablaFilas = miTabla.getElementsByTagName('tr');
+            // Se inicializa en "1" porque la primera fila no se cuenta
+            for(let i = 1; i < filas + 1; i++){
+                // Se obtiene una referencia a las celdas que se encuentren en dicha fila
+                var tablaColumnas = tablaFilas[i].getElementsByTagName('td');
+                // Se inicializa en "1" porque la primera columna no se cuenta
+                for(let j = 1; j < columnas + 1; j++){
+                    // console.log(elementos[i - 1][j - 1].x + ", " + elementos[i - 1][j - 1].y);
+                    // Se agrega el contenido a cada una de las celdas
+                    tablaColumnas[j].textContent = elementos[i - 1][j - 1].x + ", " + elementos[i - 1][j - 1].y;
+                }
+            }
+        /* ===== fin: llenar la tabla ===== */
+
+        /* ===== inicio: ENCONTRAR EQUILIBRIO DE NASH EN ESTRATEGIA PURA ===== */
         var mayoresFila = [];
         var mayoresColumna = [];
         var mayorFila;
@@ -48,7 +93,7 @@ document.getElementById('Generar').addEventListener('click', e => {
             for (let j = 0; j < columnas; ++j) {
                 if (j == 0)
                     mayorFila = elementos[i][j].y;
-                if (mayorFila < elementos[i][j].y)
+                else if(mayorFila < elementos[i][j].y)
                     mayorFila = elementos[i][j].y;
             }
             mayoresFila.push(mayorFila);
@@ -59,7 +104,7 @@ document.getElementById('Generar').addEventListener('click', e => {
             for (let i = 0; i < filas; ++i) {
                 if (i == 0)
                     mayorColumna = elementos[i][j].x;
-                if (mayorColumna < elementos[i][j].x)
+                else if (mayorColumna < elementos[i][j].x)
                     mayorColumna = elementos[i][j].x;
             }
             mayoresColumna.push(mayorColumna);
@@ -70,40 +115,24 @@ document.getElementById('Generar').addEventListener('click', e => {
         // Recorrer por columnas
         for (let i = 0; i < filas; ++i) {
             for (let j = 0; j < columnas; ++j) {
-                if (elementos[i][j].y == mayoresFila[i])
+                if (elementos[i][j].y == mayoresFila[i]){
                     console.log('indice fila: ' + j);
+                    break;   
+                }
             }
         }
         // Recorrer por filas
         for (let j = 0; j < columnas; ++j) {
             for (let i = 0; i < filas; ++i) {
-                if (elementos[i][j].x == mayoresColumna[j])
+                if (elementos[i][j].x == mayoresColumna[j]){
                     console.log('indice columna: ' + i);
+                    break;
+                }
             }
         }
         // ----fin: ENCONTRAR RESPUESTA
-        // fin: ENCONTRAR EQUILIBRIO DE NASH EN ESTRATEGIA PURA
+        /* ===== fin: ENCONTRAR EQUILIBRIO DE NASH EN ESTRATEGIA PURA ===== */
 
-        for (let i = 0; i < _tr.length; ++i) {
-            for (let j = 0; j < columnas + 1; ++j) {
-                if (i == 0) {
-                    var th = document.createElement('th');
-                    _tr[i].appendChild(th);
-                    if (j != 0) {
-                        th.textContent = "Opcion " + j;
-                    }
-                    else {
-                        th.textContent = "\t\t";
-                    }
-                }
-                else {
-                    var td = document.createElement('td');
-                    _tr[i].appendChild(td)
-                    if (j == 0) {
-                        td.textContent = "Opcion " + i;
-                    }
-                }
-            }
-        }
+        
     }
 })
